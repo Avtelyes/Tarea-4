e239f19e93c87999357912317d310a6d96120272
Avtelyes <avtelyes@gmail.com>
2015-10-30 00:56:46 -0600

    Tarea 4

--- Ejercicio4/Ejercicio4/main.c
+++ Ejercicio4/Ejercicio4/main.c
@@ -14,20 +14,20 @@
 #include <unistd.h>
 
 #define UNIDADES 4
-#define ALARMA 45
+#define ALARMA 66
+#define LECTURAS 10
 
 void * datos(void *);
 void * unidad_central(void *);
 void raise_alarm(int);
-void escritura(int );
 void apertura_archivo();
-void cerrar_archivo(int);
 int * muestreo;
 int * adquisicion;
 
 int termino = 1, conteo = 0;
 
 pthread_mutex_t * mutex_sensor;
+
 pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t muestra_cond = PTHREAD_COND_INITIALIZER;
 
@@ -42,21 +42,26 @@ int main(int argc, const char * argv[]) {
     
     sensores = (pthread_t *) malloc(sizeof(pthread_t) * UNIDADES);
     mutex_sensor = (pthread_mutex_t * ) malloc(sizeof(pthread_mutex_t) * UNIDADES);
+    
     muestreo = (int *) malloc(sizeof(int) * UNIDADES);
     adquisicion = (int *) malloc(sizeof(int) * UNIDADES);
     
     apertura_archivo();
     
-    signal(SIGINT,cerrar_archivo);
+    printf("Mi id es %d\n",getpid());
     
     int i = 0;
     
-    for (; i < UNIDADES; ++i)
+    pthread_mutex_t * aux_t;
+    
+    for (aux_t = mutex_sensor; aux_t < (mutex_sensor+UNIDADES); ++aux_t)
     {
-        pthread_mutex_init(mutex_sensor+i,NULL);
+        pthread_mutex_init(aux_t,NULL);
     }
     
     pthread_create(&UCR,NULL,unidad_central,NULL);
+    
+    pthread_t * aux;
     
     for (i = 0; i < UNIDADES; ++i)
     {
@@ -65,9 +70,9 @@ int main(int argc, const char * argv[]) {
     }
     
     
-    for (i = 0; i < UNIDADES; ++i)
+    for (aux = sensores; aux < (sensores+UNIDADES); ++aux)
     {
-        pthread_join(*(sensores+i),NULL);
+        pthread_join(*aux,NULL);
     }
     
     pthread_join(UCR,NULL);
@@ -80,33 +85,68 @@ void * unidad_central(void * arg){
     printf("---- CREANDO EL UCR \n");
     signal(SIGUSR1,raise_alarm);
     
+    printf("El pid de mi padre es: %d\n", getpid());
+    
+    int conteo = 0;
     while (termino)
     {
-        escritura(conteo);
-        printf("%Lectura realizada \n");
-        sleep(3);
+        int i = 0;
+        
+        for (; i < UNIDADES; ++i)
+        {
+            pthread_mutex_lock(mutex_sensor+i);
+            
+            fprintf(lecturas,"UAD %d: Lectura de %d\n",i,*(muestreo+i));
+            
+            pthread_mutex_unlock(mutex_sensor+i);
+        }
+        
+        printf("Lectura realizada \n");
+        sleep(5);
+        
+        ++conteo;
+        
+        if(conteo == LECTURAS)
+        {
+            fclose(lecturas);
+            free(mutex_sensor);
+            free(muestreo);
+            free(adquisicion);
+            raise(SIGTERM);
+            break;
+        }
     }
     
     pthread_exit(NULL);
 }
 
-void *datos(void *arg){
+void * datos(void *arg){
     int id = (int)arg;
-    printf("------Iniciando el sensor %d-------\n",id);
-    int random;
-    while (1)
+    printf("++++ CREANDO UAD %d \n",id);
+    
+    int tering = 1;
+    int temperatura;
+    
+    while (tering)
     {
-        random = rand() % 50;
+        temperatura = rand() % 70;
+        
         pthread_mutex_lock(mutex_sensor+id);
-        *(muestreo+id) = random;
+        
+        *(muestreo+id) = temperatura;
+        
         pthread_mutex_unlock(mutex_sensor+id);
-        if (random > ALARMA){
+        
+        if (temperatura > ALARMA)
+        {
             pthread_mutex_lock(mutex_sensor+id);
+            
             *(adquisicion+id) = 1;
             pthread_kill(UCR,SIGUSR1);
+            
             pthread_mutex_unlock(mutex_sensor+id);
         }
-        int s = sleep(1);
+        usleep(700);
     }
     pthread_exit(NULL);
 }
@@ -117,28 +157,17 @@ void raise_alarm(int id){
     while (i < UNIDADES)
     {
         pthread_mutex_lock(&mutex_sensor[i]);
-        if (*(adquisicion+i)){
+        if (*(adquisicion+i))
+        {
             pthread_mutex_lock(&file_mutex);
-            fprintf(lecturas,"Central: Recibi valor critico %d de %d\n",*(muestreo+i),i);
+            
+            fprintf(lecturas,"ALERTA: la lectura es de %d\n",*(muestreo+i));
+            
             pthread_mutex_unlock(&file_mutex);
             *(adquisicion+i) = 0;
         }
         pthread_mutex_unlock(&mutex_sensor[i]);
         ++i;
-    }
-}
-
-void escritura(int conteo)
-{
-    int i = 0;
-    
-    for (; i < UNIDADES; ++i)
-    {
-        pthread_mutex_lock(mutex_sensor+i);
-        
-        fprintf(lecturas,"Lectura del sensor %d: %d\n",i,*(muestreo+i));
-        
-        pthread_mutex_unlock(mutex_sensor+i);
     }
 }
 
@@ -151,13 +180,4 @@ void apertura_archivo()
         printf("Error opening file!\n");
         exit(1);
     }
-}
-
-void cerrar_archivo(int id)
-{
-    fclose(lecturas);
-    free(mutex_sensor);
-    free(muestreo);
-    free(adquisicion);
-    raise(SIGTERM);
 }
